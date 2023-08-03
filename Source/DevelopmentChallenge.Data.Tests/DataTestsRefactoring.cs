630682d6a19025b29a14a1d7aef5bf0de1537f0c
David Ferreira <davidferreira87@gmail.com>
2023-08-03 10:36:05 -0300

    Nuevos tests

--- Source/DevelopmentChallenge.Data.Tests/DataTestsRefactoring.cs
+++ Source/DevelopmentChallenge.Data.Tests/DataTestsRefactoring.cs
@@ -107,5 +107,39 @@ namespace DevelopmentChallenge.Data.Tests
                 "<h1>Relazione sulle forme geometriche</h1>2 Trapezi | Area 14 | Perimetro 18 <br/>2 Rettangoli | Area 8 | Perimetro 18 <br/>TOTAL:<br/>4 forme geometriche perimetro 36 area 22",
                 resumen);
         }
+
+        [TestCase]
+        public void TestResumenListaRectanguloAreaIgualCuadradoIngles()
+        {
+            //Con éste test confirmo que un rectángulo con los lados iguales es un cuadrado, y tienen el mismo valor en ingles
+            var formas = new List<IFormaGeometrica>
+            {
+                new Cuadrado(2),
+                new Rectangulo(2, 2)
+            };
+
+            var idiomaIngles = new Ingles();
+            var resumen = FormaGeometrica.Imprimir(formas, idiomaIngles);
+
+            Assert.AreEqual(
+                "<h1>Shapes report</h1>1 Square | Area 4 | Perimeter 8 <br/>1 Rectangle | Area 4 | Perimeter 8 <br/>TOTAL:<br/>2 shapes Perimeter 16 Area 8",
+                resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaRectanguloTrapecioCastellano()
+        {
+            var formas = new List<IFormaGeometrica>
+            {
+                new Trapecio(8, 5, 4, 3, 7),
+                new Rectangulo(2, 6)
+            };
+
+            var resumen = FormaGeometrica.Imprimir(formas);
+
+            Assert.AreEqual(
+                "<h1>Reporte de Formas</h1>1 Trapecio | Area 26 | Perimetro 23 <br/>1 Rectangulo | Area 12 | Perimetro 16 <br/>TOTAL:<br/>2 formas Perimetro 39 Area 38",
+                resumen);
+        }
     }
 }
